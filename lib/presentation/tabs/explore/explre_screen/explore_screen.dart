b80e160c1fd85aa79019b554a7d96d0e94e64b36
RaWan Magdy <rawan.magdy.fahmy@gmail.com>
2024-10-15 13:03:14 +0300

    UpdateCustomSearch

--- lib/presentation/tabs/explore/explre_screen/explore_screen.dart
+++ lib/presentation/tabs/explore/explre_screen/explore_screen.dart
@@ -1,13 +1,10 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_exam_app/core/styles/fonts/app_fonts.dart';
-import 'package:online_exam_app/domain/entities/GetAllSubjects.dart';
+import 'package:online_exam_app/presentation/tabs/explore/explre_screen/widgets/custom_search.dart';
 import 'package:online_exam_app/presentation/tabs/explore/explre_screen/widgets/explore_container.dart';
 import 'package:online_exam_app/presentation/tabs/explore/explore_view_model/explore_states.dart';
-
 import '../../../../core/styles/colors/app_colors.dart';
 import '../../../../di/di.dart';
 import '../explore_view_model/explore_cubit.dart';
@@ -31,40 +28,9 @@ class ExploreScreen extends StatelessWidget {
             ),
             body: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 10.h,
-                          ),
-                          backgroundColor: Colors.white,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(color: AppColors.kGray),
-                            borderRadius: BorderRadius.circular(18.r),
-                          )),
-                      onPressed: () {},
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 15),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.search,
-                              color: Colors.grey,
-                            ),
-                            Text(
-                              "Search",
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ),
-                      )),
-                ),
+                CustomSearch(),
                 SizedBox(
-                  height: 20.h,
+                  height: 15.h,
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -81,27 +47,28 @@ class ExploreScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                state is ExploreSuccessState?
-                Expanded(
-                  child: GridView.builder(
-                      shrinkWrap: true,
-                      itemCount: viewModel.subjectList.length,
-                      scrollDirection: Axis.vertical,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 1,
-                        crossAxisSpacing: 5,
-                        childAspectRatio: 3.1,
-                      ),
-                      itemBuilder: (context, index) {
-                        return ExploreContainer(
-                            subjects: viewModel.subjectList[index]);
-                      }),
-                ):
-                const Center(
-                  child: CircularProgressIndicator(
-                    color:AppColors.kBlue,
-                  ),
-                )
+                state is ExploreSuccessState
+                    ? Expanded(
+                        child: GridView.builder(
+                            shrinkWrap: true,
+                            itemCount: viewModel.subjectList.length,
+                            scrollDirection: Axis.vertical,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 1,
+                              crossAxisSpacing: 5,
+                              childAspectRatio: 3.1,
+                            ),
+                            itemBuilder: (context, index) {
+                              return ExploreContainer(
+                                  subjects: viewModel.subjectList[index]);
+                            }),
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator(
+                          color: AppColors.kBlue,
+                        ),
+                      )
               ],
             ),
           );
